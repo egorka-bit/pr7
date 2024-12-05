9d1ccb19df548cd393f2bf74e31578ac64fc9a1b
egorka-bit <trusinegor69@gmail.com>
2024-12-05 22:23:39 +0300

    практика7

---
+++ assects/js/main.js
@@ -0,0 +1,10 @@
+"use strict";
+btn.addEventListener('mousedown', function(){
+   btn.style.display = 'none';
+})
+btnr.addEventListener('mousedown', function(){
+    text.style.display = 'none';
+})
+btnr.addEventListener('mouseup', function(){
+    text.style.display = 'block';
+})
